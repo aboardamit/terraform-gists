64f0053e198f8dd052588dc5a86a15a99722746c
Amit Kumar <Amit.Kumar85@ie.ey.com>
2023-03-04 21:16:04 +0000

    Initial commit

---
+++ one-server/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+/**
+Create a new EC2instance
+*/
+resource "aws_instance" "example" {
+  ami           = "ami-006dcf34c09e50022"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}

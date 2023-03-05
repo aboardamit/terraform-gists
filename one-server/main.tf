600188acfdeb21f3d9cac9e96079a12f48c7dde7
Amit Kumar <Amit.Kumar85@ie.ey.com>
2023-03-05 10:51:05 +0000

    Update security group

--- one-server/main.tf
+++ one-server/main.tf
@@ -8,8 +8,33 @@ Create a new EC2instance
 resource "aws_instance" "example" {
   ami           = "ami-006dcf34c09e50022"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  /* when user_data is changed, terraform will terminate the instance
+     the original instance and launch a totally new one */
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+/*
+Create a security group
+*/
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
